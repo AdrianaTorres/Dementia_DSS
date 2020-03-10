09c865d82bdd933b7c880bc9140293e9b664f002
Adriana <a.torres37@usp.ceu.es>
2020-03-10 22:45:34 +0100

    second commit

---
+++ src/dementia_dss/Dementia_DSS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dementia_dss;
+
+/**
+ *
+ * @author adria
+ */
+// REMOVE THE RULES FOR READING IN CLIPS. if it appear in what we load to java, it will block it.
+// Instead of printing, we put that info in a slot in the template. (slot message) and the message is what we want to print. If we dont remove it, it will print it in the console.
+// Move the clips project to the folder of the project.
+// Eliminar el fact que crea el paciente con todo a nil.
+public class Dementia_DSS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
